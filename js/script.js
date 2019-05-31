7d4e8d1b13104826e0c9cbf1a6f1fe30f07a639b
Hubert Łepski <hubertlepsky@gmail.com>
2019-05-31 19:06:09 +0200

    Add map with markes

--- js/script.js
+++ js/script.js
@@ -1,54 +1,128 @@
+'use script';
+
 (function () {
 
     var object = document.getElementById('pictures').innerHTML;
     var carousel = document.querySelector('.main-carousel');
     var copyHere = '';
 
-    var data = [{
+    var data = [
+        {
             image: 'https://images.unsplash.com/photo-1526048516912-b0432b1633ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2029&q=90',
             id: 'carousel-cell1',
+            coords: {
+                lat: 41.381,
+                lng: 2.122
+            },
             title: 'Picture 1',
             description: '<p class="container description">Description 1</p>',
         },
         {
             image: 'https://images.unsplash.com/photo-1544366981-2150548c9c1c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=90',
             id: 'carousel-cell2',
+            coords: {
+                lat: 41.379,
+                lng: 2.117
+            },
             title: 'Picture 2',
             description: '<p class="container description">Description 2</p>',
         },
         {
-            image: 'https://images.unsplash.com/photo-1529551739587-e242c564f727?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1490&q=90',
+            image: 'https://images.unsplash.com/photo-1533676802871-eca1ae998cd5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1491&q=90',
             id: 'carousel-cell3',
+            coords: {
+                lat: 45.431,
+                lng: 12.335
+            },
             title: 'Picture 3',
             description: '<p class="container description">Description 3</p>',
         },
         {
             image: 'https://images.unsplash.com/photo-1475483768296-6163e08872a1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=90',
             id: 'carousel-cell4',
+            coords: {
+                lat: 51.103,
+                lng: 17.075
+            },
             title: 'Picture 4',
             description: '<p class="container description">Description 4</p>',
         },
         {
             image: 'https://images.unsplash.com/photo-1470219556762-1771e7f9427d?ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=90',
             id: 'carousel-cell5',
+            coords: {
+                lat: 40.756,
+                lng: -73.979
+            },
             title: 'Picture 5',
             description: '<p class="container description">Description 5</p>',
         }
     ];
 
+
+    // MAPKA GOOGLE new
+    var firstPlace;
+    var secondPlace;
+    var thirdPlace;
+    var fourthPlace;
+    var fifthPlace;
+
+    for (var i = 0; i < data.length; i++) {
+        firstPlace = data[0].coords;
+        secondPlace = data[1].coords;
+        thirdPlace = data[2].coords;
+        fourthPlace = data[3].coords;
+        fifthPlace = data[4].coords;
+    }
+
+    // MAPKA GOOGLE
+    window.initMap = function () {
+
+        // The map, centered at coords1
+        var map = new google.maps.Map(
+            document.getElementById('map'), {
+                zoom: 3,
+                center: firstPlace
+            });
+
+        // The marker, positioned at coords1
+        var markerOne = new google.maps.Marker({
+            position: firstPlace,
+            map: map
+        });
+
+        var markerTwo = new google.maps.Marker({
+            position: secondPlace,
+            map: map
+        });
+
+        var markerThree = new google.maps.Marker({
+            position: thirdPlace,
+            map: map
+        });
+
+        var markerFour = new google.maps.Marker({
+            position: fourthPlace,
+            map: map
+        });
+
+        var markerFive = new google.maps.Marker({
+            position: fifthPlace,
+            map: map
+        });
+    }
+
+
     Mustache.parse(object);
 
     for (var i = 0; i < data.length; i++) {
-        console.log(data);           // opcjonalnie - pomocne
+        console.log(data); // opcjonalnie - pomocne
         copyHere += Mustache.render(object, data[i]);
     }
-
-    console.log(carousel);
 
     carousel.insertAdjacentHTML('beforeend', copyHere);
 
 })();
-
 
 
 var flkty = new Flickity('.main-carousel', {
