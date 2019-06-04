9f0f7ef04ccfdaff423fecd5aa5967c153978564
Hubert Łepski <hubertlepsky@gmail.com>
2019-06-05 00:09:59 +0200

    Connection of plugins

--- js/script.js
+++ js/script.js
@@ -7,54 +7,54 @@
     var copyHere = '';
 
     var data = [{
-            image: 'https://images.unsplash.com/photo-1526048516912-b0432b1633ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2029&q=90',
+            image: 'https://images.unsplash.com/photo-1513635269975-59663e0ac1ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=70',
             id: 'carousel-cell1',
             coords: {
-                lat: 41.381,
-                lng: 2.122
+                lat: 51.505,
+                lng: -0.077
             },
             title: 'Picture 1',
-            description: '<p class="container description">Description 1</p>',
+            description: '<p class="container description">London</p>',
         },
         {
-            image: 'https://images.unsplash.com/photo-1544366981-2150548c9c1c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=90',
+            image: 'https://images.unsplash.com/photo-1544366981-2150548c9c1c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=70',
             id: 'carousel-cell2',
             coords: {
                 lat: 41.379,
                 lng: 2.117
             },
             title: 'Picture 2',
-            description: '<p class="container description">Description 2</p>',
+            description: '<p class="container description">Barcelona</p>',
         },
         {
-            image: 'https://images.unsplash.com/photo-1533676802871-eca1ae998cd5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1491&q=90',
+            image: 'https://images.unsplash.com/photo-1533676802871-eca1ae998cd5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1491&q=80',
             id: 'carousel-cell3',
             coords: {
                 lat: 45.431,
                 lng: 12.335
             },
             title: 'Picture 3',
-            description: '<p class="container description">Description 3</p>',
+            description: '<p class="container description">Venice</p>',
         },
         {
-            image: 'https://images.unsplash.com/photo-1475483768296-6163e08872a1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=90',
+            image: 'https://images.unsplash.com/photo-1475483768296-6163e08872a1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=50',
             id: 'carousel-cell4',
             coords: {
                 lat: 51.103,
                 lng: 17.075
             },
             title: 'Picture 4',
-            description: '<p class="container description">Description 4</p>',
+            description: '<p class="container description">Wroclaw</p>',
         },
         {
-            image: 'https://images.unsplash.com/photo-1470219556762-1771e7f9427d?ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=90',
+            image: 'https://images.unsplash.com/photo-1470219556762-1771e7f9427d?ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=70',
             id: 'carousel-cell5',
             coords: {
                 lat: 40.756,
                 lng: -73.979
             },
             title: 'Picture 5',
-            description: '<p class="container description">Description 5</p>',
+            description: '<p class="container description">New York</p>',
         }
     ];
 
@@ -78,8 +78,21 @@
                 map: map
             });
 
-        }
-    }
+            markers[i].addListener('click', function () {
+                flkty.select(i);
+            })
+
+
+            flkty.on('change', function (i) {
+
+                //  wykorzystujemy metodę panTo w obiekcie map do przesunięcia współrzędnych mapy
+                map.panTo(data[i].coords);
+
+                //  następnie zmieniamy powiększenie mapy
+                map.setZoom(6);
+            });
+        };
+    };
 
 
     // SECTION  CAROUSEL
